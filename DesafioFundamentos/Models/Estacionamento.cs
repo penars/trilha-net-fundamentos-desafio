646cf62e80d64d519c8ecbb26adc328bc7185ce5
Pena <pena.drescher@gmail.com>
2025-08-06 12:48:12 -0300

    Desafio concluido

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -45,6 +45,7 @@ namespace DesafioFundamentos.Models
             }
             else
             {
+                
                 Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
             }
         }
